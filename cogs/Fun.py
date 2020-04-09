291cd993158bd0aba6af8683c50b073c3148639c
Conner Wolf  08 <41162825+Connerwolf@users.noreply.github.com>
2020-04-09 19:06:23 +0530

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import random
+import praw
 
 class Fun(commands.Cog):
     def __init__(self, client):
@@ -45,6 +46,86 @@ class Fun(commands.Cog):
         await ctx.send(embed=flip)
         time.sleep(2)
         await ctx.send(embed=result)
+
+    @commands.command()
+    async def meme(self, ctx):
+        load = discord.Embed(
+            title = 'Loading...',
+            color = discord.Color.green()
+        )
+        await ctx.send(embed=load, delete_after=1)
+
+        reddit =  praw.Reddit(client_id = 'cKtdy4mx7fPopQ',
+                        client_secret = 'PuQBb-kfu6EPxayDZQYqydYq6v0',
+                        username = 'Connerwolf08',
+                        password = 'Sonu ghost@123',
+                        user_agent = 'Winter08 v.1.0'
+        )
+
+        memes_submissions = reddit.subreddit('memes').hot()
+        post_to_pick = random.randint(1, 100)
+        for x in range(0, post_to_pick):
+            submission = next(x for x in memes_submissions if not x.stickied)
+        meme = discord.Embed(
+            title = f'``Title :``{submission.title}',
+            color = discord.Color.green()
+        )
+        meme.set_image(url=f'{submission.url}')
+        meme.add_field(name=':thumbsup: **Upvotes** :',value= f'{submission.ups}',inline=True)
+        meme.add_field(name=':thumbsdown: **Downvotes** :',value= f'{submission.downs}',inline=True)
+        await ctx.send(embed=meme)
+
+    @commands.command()
+    async def cat(self, ctx):
+        load = discord.Embed(
+            title = 'Loading...',
+            color = discord.Color.green()
+        )
+        await ctx.send(embed=load, delete_after=1)
+
+        reddit =  praw.Reddit(client_id = 'cKtdy4mx7fPopQ',
+                        client_secret = 'PuQBb-kfu6EPxayDZQYqydYq6v0',
+                        username = 'Connerwolf08',
+                        password = 'Sonu ghost@123',
+                        user_agent = 'Winter08 v.1.0'
+        )
+        cat_images = reddit.subreddit('cats').hot()
+        post_to_pick = random.randint(1, 100)
+        for x in range(0, post_to_pick):
+            submission = next(x for x in cat_images if not x.stickied)
+        cat = discord.Embed(
+            title = f'``Here take some Cats. `` :heart_eyes_cat:',
+            color = discord.Color.green()
+        )
+        cat.set_image(url=f'{submission.url}')
+        await ctx.send(embed=cat)
+
+    @commands.command()
+    async def dog(self, ctx):
+        load = discord.Embed(
+            title = 'Loading...',
+            color = discord.Color.green()
+        )
+        await ctx.send(embed=load, delete_after=1)
+
+        reddit =  praw.Reddit(client_id = 'cKtdy4mx7fPopQ',
+                        client_secret = 'PuQBb-kfu6EPxayDZQYqydYq6v0',
+                        username = 'Connerwolf08',
+                        password = 'Sonu ghost@123',
+                        user_agent = 'Winter08 v.1.0'
+        )
+        dog_images = reddit.subreddit('dogpictures').hot()
+        post_to_pick = random.randint(1, 100)
+        for x in range(0, post_to_pick):
+            submission = next(x for x in dog_images if not x.stickied)
+        dog = discord.Embed(
+            title = f'``Here take some Dogs. `` :dog:',
+            color = discord.Color.green()
+        )
+        dog.set_image(url=f'{submission.url}')
+        await ctx.send(embed=dog)
+
+        
 
 def setup(client):
     client.add_cog(Fun(client))
